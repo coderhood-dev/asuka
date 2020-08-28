47f3d7cac70fbfffff03e17ec1f3d5f7a7d936a1
Pereiro Antonio <tonoezep@gmail.com>
2020-08-28 13:55:20 -0300

    Simplify roll (#4)
    * Simplify roll import
    
    * Simplify Discord import too
    
    * Missing file
    
    Co-authored-by: Antonio Pereiro <apereiro.sow@ten-x.com>

--- commands/roll.js
+++ commands/roll.js
@@ -1,7 +1,10 @@
+const Roll = require('roll')
+const roll = new Roll();
+
 module.exports = {
 	name: 'roll',
 	description: 'Display a result element from.',
-	execute(message, args, client, roll) {
+	execute(message, args) {
         const attack = roll.roll(args[0])
 		message.channel.send(`Executed ${args[0]}\nResult:${attack.result}`);
 	},
