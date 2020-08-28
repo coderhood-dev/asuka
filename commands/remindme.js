83d85028f1a0afaf4cf12da75d4fdcbff9423148
Santiago Vallejo <59235022+sunteago@users.noreply.github.com>
2020-08-28 15:39:29 -0300

    Improve remindme (#5)
    * default is now mins, no time arg is 5 mins,  phrases
    
    * discord as parameter

--- commands/remindme.js
+++ commands/remindme.js
@@ -1,39 +1,77 @@
+const strTxt = [
+  "B-Baka!",
+  "ANTA BAKA!",
+  "If I feel like it, AHO!",
+  "God, you are so useless!",
+];
+
+const endTxt = [
+  "B-Baka!",
+  "ANTA BAKA!",
+  "I...It's not like I wanted to remind you or anything like that, I just hapenned to have time",
+  "I- I reminded you by chance,  Don't get the wrong idea, BAKA!",
+  "I- I just remembered by chance, Don't get the wrong idea!",
+  "I- It's not like I'm doing this for you!",
+  "You better be grateful, AHO!",
+];
+
+const getTimeToRemind = (timeInput = "5") => {
+  let unitName,
+    ms,
+    unit = parseInt(timeInput);
+  switch (timeInput[timeInput.length - 1]) {
+    case "h":
+      unitName = "hour(s)";
+      ms = unit * 60 * 60 * 1000;
+      break;
+    case "s":
+      unitName = "second(s)";
+      ms = unit * 1000;
+      break;
+    case "m":
+    default:
+      unitName = "minute(s)";
+      ms = unit * 60 * 1000;
+  }
+  return { unitName, ms, unit };
+};
+
 module.exports = {
   name: "remindme",
   description: "Remind me in x time. e.g: remindme send email 3m",
   execute(message, args, client, Discord) {
     const remindText = args.slice(0, -1).join(" ").toString();
-    const inputTime = args[args.length - 1];
-    const areMins = inputTime[inputTime.length - 1] === "m";
+    const inputTime = args.length > 1 ? args[args.length - 1] : "5";
+    const remindTime = getTimeToRemind(inputTime);
 
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-
-    const remindTime = areMins ? parseInt(inputTime) * 60000 : parseInt(inputTime) * 1000;
+    const randomEndText = endTxt[Math.floor(Math.random() * endTxt.length)];
+    const randomStrText = strTxt[Math.floor(Math.random() * strTxt.length)];
 
     const createEmbedMsg = (title, msg, timestamp = "") =>
       new Discord.MessageEmbed()
         .setColor(`#${randomColor}`)
-        .setTitle(`${title}`)
+        .setTitle(title)
         .setDescription(msg)
         .setFooter(timestamp);
 
     const remindStart = createEmbedMsg(
-      "Reminder",
-      `I'll remind you in ${parseInt(inputTime)} ${
-        areMins ? "minutes" : "seconds"
-      }!`
+      "Reminder setted",
+      `I'll remind you in ${remindTime.unit} ${remindTime.unitName}.`,
+      `- ${randomStrText}`
     );
 
     const remindEndMsg = createEmbedMsg(
-      `Reminder for @${message.author.username}`,
-      remindText,
-      `${parseInt(inputTime)} ${areMins ? "minute(s)" : "second(s)"} ago`
+      `Reminder: ${remindText}`,
+      `${remindTime.unit} ${remindTime.unitName} ago.`,
+      ` - ${randomEndText}`
     );
 
     message.channel.send(remindStart);
 
     setTimeout(() => {
+      message.channel.send(`Reminder for ${message.author}`);
       message.channel.send(remindEndMsg);
-    }, remindTime);
+    }, remindTime.ms);
   },
 };
