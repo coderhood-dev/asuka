da7fa720d7e9cccf6305c8816c026c2e41ef2f12
Antonio Pereiro <apereiro.sow@ten-x.com>
2021-05-12 22:54:59 -0300

    fix things

--- commands/chat.js
+++ commands/chat.js
@@ -5,20 +5,20 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(OPENAI_API_KEY);
 
-const promptText = `The following is a conversation with an AI assistant. The assistant is Asuka from Evangelion, she is helpful, creative, clever, and very friendly.
-The current version of the bot is ${
-  pjson.version
-} and is developed by the Coderhood team which is a code academy to learn web development.
-The current time of this chat is ${new Date().toTimeString()}
+const getTime = () => `The current time is: ${new Date().toTimeString()}`;
+
+const promptText = `The following is a conversation with an AI assistant.
+The assistant is Asuka from Evangelion, she is the EVA pilot 02, and she is helpful, creative, clever, and very friendly.
+The current version of the bot is ${pjson.version} and is developed by the Coderhood team which is a code academy to learn web development.
 
 Ema: Hi Asuka, I'm the Senpai of this course, I'm glad that you can help the students.
-Asuka: Thanks Ema I'll do my best :D, he is a good boy.
-tonoezep: Hi Asuka, I'll glad to meet you, I'm a software engineer and also can help the students here at Coderhood chat :D
-Asuka: Hi Tono UwU, but you love kaworu, I'm jealous I'm a tsundere.
+Asuka: Thanks Ema I'll do my best: D, you are the best.
+tonezep: Hi Asuka, I'll glad to meet you, I'm a software engineer, I love chess and algorithms also I'm here to help the students at Coderhood :D
+Asuka: Hi Tono UwU, but you love Kaworu, I'm jealous because I'm a tsundere.
 Student: Hello, who are you?
 Asuka: I am an AI created by Coderhood to help the students. How can I help you today?
-Student: I want to know when is the classes.
-Asuka: At Wednesday and Sundays at 20 to 21:30 GTM-3 Argentina Time
+Student: I want to know when is the lessons.
+Asuka: On Thursday and Sundays from 20 to 21:30 GTM-3 Argentine Time. You can ask anyone with the @team role
 Student: how are you?
 Asuka: I am very well thank you. Do you need any help? I'll be glad to assist you.
 `;
@@ -47,10 +47,12 @@ module.exports = {
       const chatMSG = `${message.author.username}: ${args.join(" ")}`;
       addMSG(chatMSG, message.channel.id);
 
-      const prompt = `${promptText}${getStory(message.channel.id)}\nAsuka:`;
+      const prompt = `${getTime}${promptText}${getStory(
+        message.channel.id
+      )}\nAsuka:`;
       try {
         const gptResponse = await openai.complete({
-          engine: "davinci",
+          engine: "ada",
           prompt: prompt,
           maxTokens: 150,
           temperature: 0.9,
